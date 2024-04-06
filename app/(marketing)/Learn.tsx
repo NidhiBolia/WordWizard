bd9fe4f0fda3abf36c57307a6197218c010ddae6
NidhiBolia <nidhibolia.cse21@aii.ac.in>
2024-04-06 20:33:23 +0530

    deployemnt-error

--- app/(marketing)/Learn.tsx
+++ app/(marketing)/Learn.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 
-const Learn: React.FC = () => {
+export default function Learn() {
   const Boxes = [
     { id: 0, text: 'Translation' },
     { id: 1, text: 'Listening comprehension' },
@@ -9,13 +8,12 @@ const Learn: React.FC = () => {
     { id: 4, text: 'Master everyday conversations, essential phrases, and cultural insights' },
     { id: 5, text: ' Expand your language skills in contexts relevant to your personal aspirations'}
   ];
-
   return (
     <div className="mx-w-8xl mx-auto py-20 px-8">
       <h2 className="text-4xl font-bold mb-8">What will you learn</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {Boxes.map((box) => (
-          <div key={box.id} className="flex flex-col text-gray-600 bg-gray-50 rounded-lg">
+          <div key={box.id} className="fex flex-col text-gray-600 bg-gray-50 rounded-lg">
             <div className="py-3 px-3 flex items-center justify-between border-2 border-gray-500 rounded-t-lg text-gray-700 font-bold">
               <div className="flex space-x-1.5">
                 <div className="w-3 h-3 rounded-full bg-red-500"></div>
@@ -29,6 +27,4 @@ const Learn: React.FC = () => {
       </div>
     </div>
   );
-};
-
-export default Learn;
+}
